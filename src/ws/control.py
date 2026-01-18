26a4b3d6332fbf8924add8b281a24c79d2bc9a6c
yusshu <andreroldan.me@gmail.com>
2026-01-18 18:50:20 -0500

    fix: fix how we access device id

--- src/ws/control.py
+++ src/ws/control.py
@@ -10,7 +10,7 @@ class ControlConnection(Connection):
         if cmd == "DEVICE":
             device_id, device_cmd, *device_args = args
             for conn in self.server.connections.values():
-                if conn.type == "device" and conn.device["id"] == device_id:
+                if conn.type == "device" and conn.device.id == device_id:
                     return await conn.handle_from_client(self, device_cmd, device_args)
             return True
         return False
